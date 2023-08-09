62b81e9105a19acf48a3c27999c09ea7a08333c0
jicruz96 <accounts@jicruz.com>
2023-08-08 21:48:00 -0400

    update test template

--- client/test/widget_test.dart
+++ client/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:scrabble_scorer/main.dart';
+import 'package:scrabble_scorer/scrabble_scorer.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ScrabbleScorer());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
