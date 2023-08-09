8c1dc6568812ab0e070ccfac3710771fa527fd3e
jicruz96 <accounts@jicruz.com>
2023-08-08 22:06:17 -0400

    remove unnecessary getter in ScrabbleKeyboardState

--- client/lib/scrabble_keyboard.dart
+++ client/lib/scrabble_keyboard.dart
@@ -2,17 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:scrabble_scorer/scrabble_key.dart';
 
 class ScrabbleKeyboardState extends ChangeNotifier {
-  String _typedText = '';
-
-  String get typedText => _typedText;
+  String typedText = '';
 
   void type(String letter) {
     if (letter == '<') {
-      if (_typedText.length > 0) {
-        _typedText = _typedText.substring(0, _typedText.length - 1);
+      if (typedText.isNotEmpty) {
+        typedText = typedText.substring(0, typedText.length - 1);
       }
     } else {
-      _typedText += letter;
+      typedText += letter;
     }
     notifyListeners();
   }
