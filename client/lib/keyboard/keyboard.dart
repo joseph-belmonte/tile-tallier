ec41ac3b5bdd9f2d8beab54ed7d33c62c26ae6c0
jicruz96 <accounts@jicruz.com>
2023-08-16 20:41:35 -0400

    also keyboard toggler

--- client/lib/keyboard/keyboard.dart
+++ client/lib/keyboard/keyboard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../writing_zone.dart';
 import 'device_keyboard.dart';
+import 'keyboard_toggler.dart';
 import 'scrabble_keyboard.dart';
 
 class KeyboardWidget extends StatefulWidget {
@@ -40,57 +41,6 @@ class KeyboardWidgetState extends State<KeyboardWidget> {
           keyboardWidget,
         ],
       ),
-    );
-  }
-}
-
-/// A stupid pink bar that displays stuff. Currently it just contains
-/// a toggle button that changes the keyboard display.
-class KeyboardTogglerContainer extends StatelessWidget {
-  final KeyboardWidgetState keyboardWidgetState;
-
-  const KeyboardTogglerContainer(this.keyboardWidgetState, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.pink.shade200,
-      padding: EdgeInsets.all(5),
-      width: double.infinity,
-      child: KeyboardToggler(keyboardWidgetState),
-    );
-  }
-}
-
-class KeyboardToggler extends StatefulWidget {
-  final KeyboardWidgetState keyboardWidgetState;
-
-  const KeyboardToggler(this.keyboardWidgetState, {super.key});
-
-  @override
-  State<KeyboardToggler> createState() =>
-      _KeyboardTogglerState(keyboardWidgetState);
-}
-
-class _KeyboardTogglerState extends State<KeyboardToggler> {
-  final KeyboardWidgetState keyboardWidgetState;
-  bool switchState = false;
-
-  _KeyboardTogglerState(this.keyboardWidgetState);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Switch(
-          value: switchState,
-          onChanged: (value) {
-            keyboardWidgetState.toggleKeyboard();
-            switchState = value;
-          },
-        ),
-        Text('Change Keyboard', style: TextStyle(color: Colors.white)),
-      ],
     );
   }
 }
