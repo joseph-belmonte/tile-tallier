dede810b82b995b0643ce5c216e358e0dd668cef
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-08-18 15:21:29 -0400

    Update game_state.dart
    add doc comments for class definitions

--- client/lib/models/game_state.dart
+++ client/lib/models/game_state.dart
@@ -1,9 +1,11 @@
 class GameState {
+  /// Creates a new game state with the given players.
   GameState({required this.players});
   final List<Player> players;
 }
 
 class Player {
+  /// Creates a new player with the given name and an empty list of plays.
   Player({required this.name});
 
   final String name;
@@ -11,6 +13,9 @@ class Player {
 }
 
 class Play {
+  /// Accepts a list of PlayedWord objects and a boolean value for whether or not the play is a bingo.
+  /// A word is a list of PlayedLetter objects.
+  /// A bingo is when a player uses all 7 letters in their rack in a single turn.
   Play({required this.playedWords, required this.isBingo});
 
   List<PlayedWord> playedWords;
@@ -18,6 +23,7 @@ class Play {
 }
 
 class PlayedWord {
+  /// Accepts a word (list of PlayedLetter objects) and a boolean value for whether or not the word is a double or triple word.
   PlayedWord({
     required this.word,
     required this.isDouble,
@@ -30,6 +36,7 @@ class PlayedWord {
 }
 
 class PlayedLetter {
+  /// Accepts a letter (String) and a boolean value for whether or not the letter is a double or triple letter.
   PlayedLetter({
     required this.letter,
     required this.isDouble,
