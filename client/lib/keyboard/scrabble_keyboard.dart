a8581e1036a15d05719f5be3d42f28d81c6c273f
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-08-21 15:55:36 -0400

    wrap both keyboards with align
    this doesn't seem to "stick" to bottom of the screen so need to investigate that

--- client/lib/keyboard/scrabble_keyboard.dart
+++ client/lib/keyboard/scrabble_keyboard.dart
@@ -14,13 +14,17 @@ class ScrabbleKeyboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: keyboardRows.map((row) {
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: row.split('').map((letter) => ScrabbleKey(letter)).toList(),
-        );
-      }).toList(),
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Column(
+        children: keyboardRows.map((row) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:
+                row.split('').map((letter) => ScrabbleKey(letter)).toList(),
+          );
+        }).toList(),
+      ),
     );
   }
 }
