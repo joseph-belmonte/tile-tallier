afd8996e78952ec93abb2b1d20914055040569fc
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-08-21 21:32:39 -0400

    Update display_zone.dart
    minor ui improvement: turn # and score line up with first word when playing multiple words in the same turn

--- client/lib/display_zone.dart
+++ client/lib/display_zone.dart
@@ -13,7 +13,6 @@ class DisplayZone extends StatelessWidget {
     var players = notifier.gameState.players;
 
     // Create a map to track the turn count for each player
-    // TODO: make this a property on the Player class
     final Map<Player, int> playerTurnCounts = {};
 
     return Align(
@@ -21,34 +20,35 @@ class DisplayZone extends StatelessWidget {
       child: SingleChildScrollView(
         clipBehavior: Clip.hardEdge,
         scrollDirection: Axis.vertical,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            for (var player in players)
-              Container(
-                padding: EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 1,
-                  ),
-                ),
-                margin: EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    Text(
-                      '${player.name}: ${player.score}',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black54,
-                      ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              for (var player in players)
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1,
                     ),
-                    Expanded(
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
+                  ),
+                  margin: EdgeInsets.all(10),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${player.name}: ${player.score}',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      Expanded(
                         child: ListView.builder(
                           shrinkWrap: true,
                           itemCount: player.plays.length,
@@ -58,40 +58,31 @@ class DisplayZone extends StatelessWidget {
                             final turnCount = playerTurnCounts[player] ?? 0;
                             playerTurnCounts[player] = turnCount + 1;
                             return Row(
-                              mainAxisSize: MainAxisSize.min,
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                for (var playedWord in currentTurn.playedWords)
-                                  if (playedWord ==
-                                      currentTurn.playedWords.first)
-                                    Expanded(
-                                      child: Row(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            'Turn ${turnCount + 1}: ',
-                                            textAlign: TextAlign.left,
-                                            style: TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.w500,
-                                              color: Colors.black87,
-                                            ),
-                                          ),
-                                          Text(
-                                            currentTurn.score.toString(),
-                                            style: TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.w500,
-                                              color: Colors.red,
-                                            ),
-                                          ),
-                                        ],
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Turn ${turnCount + 1}: ',
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.black87,
                                       ),
-                                    )
-                                  else
-                                    Container(),
-                                // display each word, and its individual score
+                                    ),
+                                    Text(
+                                      currentTurn.score.toString(),
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.red,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                                 Text(
                                   currentTurn.playedWords
                                       .map((e) => '${e.word} - ${e.score}')
@@ -107,11 +98,11 @@ class DisplayZone extends StatelessWidget {
                           },
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
