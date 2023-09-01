cfae795aa682af218f82def5544fe5efaf476377
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-09-01 16:01:57 -0400

    Update widget_test.dart
    set up test (not working)

--- client/test/widget_test.dart
+++ client/test/widget_test.dart
@@ -10,20 +10,38 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:scrabble_scorer/scrabble_scorer.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Word submits smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const ScrabbleScorer());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that our beginning playedWordState is empty.
+    expect(find.text(''), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Click the letters P O O L on the keyboard
+    await tester.tap(find.text('P'));
+    await tester.tap(find.text('O'));
+    await tester.tap(find.text('O'));
+    await tester.tap(find.text('L'));
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Click on P in the playedWord to trigger a double letter score
+    await tester.tap(find.text('P'));
+    // Click the toggle multiplier button to trigger a double word score
+    await tester.tap(find.byIcon(Icons.multiple_stop_rounded));
+    // Click the submit button to play the word
+    await tester.tap(find.byIcon(Icons.add_circle_outline));
+    // Click the end turn button to switch players
+    await tester.tap(find.byIcon(Icons.switch_account_rounded));
+
+    // Verify that:
+    // - the first player has a play whose playedWords is POOL
+    expect(find.text('POOL'), findsOneWidget);
+    // - the first player has a play whose score is 18
+    expect(find.text('POOL'), findsOneWidget);
+    // - the first and second player have a turn label
+    expect(find.text('Turn 1:'), findsWidgets);
+    // - the first player has a score of 18
+    expect(find.text('Turn 1:18'), findsOneWidget);
+    // - the second player has no score
+    expect(find.text('Turn 1:0'), findsOneWidget);
   });
 }
