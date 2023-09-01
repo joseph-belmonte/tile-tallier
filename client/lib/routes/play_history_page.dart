a68cbb983aadaea3f73fcd9311e057adec1f29d2
jicruz96 <accounts@jicruz.com>
2023-09-01 15:21:02 -0400

    initial implementation of edit history

--- client/lib/routes/play_history_page.dart
+++ client/lib/routes/play_history_page.dart
@@ -5,32 +5,68 @@ import '../current_game_state.dart';
 import '../models/game_state.dart';
 import '../scrabble_letterbox.dart';
 
-class PlayHistoryPage extends StatelessWidget {
+class PlayHistoryPage extends StatefulWidget {
   const PlayHistoryPage({Key? key}) : super(key: key);
+
+  @override
+  PlayHistoryPageState createState() => PlayHistoryPageState();
+}
+
+class PlayHistoryPageState extends State<PlayHistoryPage> {
+  bool interactive;
+
+  PlayHistoryPageState({this.interactive = false});
 
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: Provider.of<CurrentGameState>(context)
-          .gameState
-          .plays
-          .map((play) => SinglePlayHistoryWidget(play, play.player))
-          .toList(),
+      children: [
+        ...getPlayHistoryEditButtonWidgets(),
+        ...getPlayHistoryWidgets(context),
+      ],
     );
+  }
+
+  List<Widget> getPlayHistoryEditButtonWidgets() {
+    return [
+      ElevatedButton(
+        onPressed: () => setState(() => interactive = !interactive),
+        child: Text(interactive ? 'Done' : 'Edit History'),
+      )
+    ];
+  }
+
+  List<SinglePlayHistoryWidget> getPlayHistoryWidgets(BuildContext context) {
+    return Provider.of<CurrentGameState>(context)
+        .gameState
+        .plays
+        .map(
+          (play) => SinglePlayHistoryWidget(
+            play,
+            play.player,
+            interactive: interactive,
+          ),
+        )
+        .toList();
   }
 }
 
 class SinglePlayHistoryWidget extends StatelessWidget {
   final Play play;
   final Player player;
+  final bool interactive;
 
-  const SinglePlayHistoryWidget(this.play, this.player, {Key? key})
-      : super(key: key);
+  const SinglePlayHistoryWidget(
+    this.play,
+    this.player, {
+    this.interactive = false,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var display = play.playedWords
-        .map((word) => ScrabbleWordWidget(word, interactive: false))
+        .map((word) => ScrabbleWordWidget(word, interactive: interactive))
         .toList();
 
     return Container(
