cd7128fc573e841fbdf540781a3888eded1b7fe3
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-09-05 11:26:36 -0400

    address review comments

--- client/lib/routes/settings_page.dart
+++ client/lib/routes/settings_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:settings_ui/settings_ui.dart';
 
+import '../keyboard.dart';
 import '../scrabble_scorer.dart';
 
 class SettingsPage extends StatelessWidget {
