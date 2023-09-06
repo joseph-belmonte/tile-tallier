d632a946cb170996b08e3c125a418718e52ee6de
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-09-06 09:31:09 -0400

    Update end_game_page.dart
    add stats alert dialog on click

--- client/lib/routes/end_game_page.dart
+++ client/lib/routes/end_game_page.dart
@@ -6,18 +6,65 @@ class EndGamePage extends StatelessWidget {
   const EndGamePage({
     required this.winner,
     required this.rankedPlayers,
+    required this.game,
     super.key,
   });
 
+  final Game game;
   final Player winner;
   final List<Player> rankedPlayers;
 
   showStatsDialog(BuildContext context) {
+    String shortestWord = rankedPlayers[0].shortestWord;
+    String longestWord = rankedPlayers[0].longestWord;
+    PlayedWord highestScoringWord = rankedPlayers[0].highestScoringWord;
+    Play highestScoringTurn = rankedPlayers[0].highestScoringTurn;
+
+    for (final player in rankedPlayers) {
+      if (player.longestWord.length > longestWord.length) {
+        longestWord = player.longestWord;
+      }
+      if (player.highestScoringWord.score > highestScoringWord.score) {
+        highestScoringWord = player.highestScoringWord;
+      }
+      if (player.highestScoringTurn.score > highestScoringTurn.score) {
+        highestScoringTurn = player.highestScoringTurn;
+      }
+      if (player.shortestWord.length < shortestWord.length) {
+        shortestWord = player.shortestWord;
+      }
+    }
+
     return showDialog(
       context: context,
       builder: (context) => AlertDialog(
         title: Text('Game Statistics'),
-        content: Text('This is where the stats will go'),
+        content: Container(
+          decoration: ShapeDecoration(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: <Widget>[
+                Text('Total Plays: ${game.plays.length}'),
+                SizedBox(height: 20),
+                Text('Longest Word: $longestWord'),
+                SizedBox(height: 20),
+                Text('Highest Scoring Word: ${highestScoringWord.word}'),
+                SizedBox(height: 20),
+                Text(
+                  'Highest Scoring Turn: ${highestScoringTurn.score} - ${highestScoringTurn.playedWords.map((word) => word.word)}',
+                ),
+                SizedBox(height: 20),
+                Text('Shortest Word: $shortestWord'),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
