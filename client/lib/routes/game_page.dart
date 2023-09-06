f2c2cb47e4a62e30edd82873bde9c8d1d08973fe
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-09-06 09:34:49 -0400

    rename scorepage to gamepage

--- client/lib/routes/game_page.dart
+++ client/lib/routes/game_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:scrabble_scorer/display_zone.dart';
 import 'package:scrabble_scorer/writing_zone.dart';
 
-class ScorePage extends StatelessWidget {
-  const ScorePage({super.key});
+class GamePage extends StatelessWidget {
+  const GamePage({super.key});
 
   @override
   Widget build(BuildContext context) {
