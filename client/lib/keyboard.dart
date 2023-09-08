1f6259d3cc846dcfd469eb8f44e54e6d65c2a44d
jicruz96 <accounts@jicruz.com>
2023-09-08 01:39:21 -0400

    use underscore for enter

--- client/lib/keyboard.dart
+++ client/lib/keyboard.dart
@@ -76,9 +76,9 @@ class ButtonKeyboard extends StatelessWidget {
   static const List<String> keyboardRows = [
     'QWERTYUIOP',
     'ASDFGHJKL',
-    // The '/' is a placeholder for the return key
+    // The '_' is a placeholder for the return key
     // The '<' is a placeholder for the backspace key
-    '/ ZXCVBNM<',
+    '_ ZXCVBNM<',
   ];
 
   const ButtonKeyboard({super.key});
@@ -105,7 +105,7 @@ class KeyboardKey extends StatelessWidget {
   const KeyboardKey(this.value, {super.key});
 
   Widget get icon {
-    if (value == '/') return Icon(Icons.keyboard_return);
+    if (value == '_') return Icon(Icons.keyboard_return);
     if (value == '<') return Icon(Icons.backspace);
     if (value == ' ') return Icon(Icons.space_bar);
     return Text(value, style: const TextStyle(fontSize: 20));
@@ -113,7 +113,7 @@ class KeyboardKey extends StatelessWidget {
 
   void Function() getOnTapBehavior(BuildContext context) {
     var playedWordState = Provider.of<CurrentPlayState>(context, listen: false);
-    if (value == '/') return () => playedWordState.playWord(context);
+    if (value == '_') return () => playedWordState.playWord(context);
     if (value == '<') return () => playedWordState.removeLetter();
     return () => playedWordState.playLetter(value);
   }
