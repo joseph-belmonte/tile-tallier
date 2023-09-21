20ea1491eac714b3d9ef051a1768b632aa20f6ea
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-09-21 11:17:53 -0400

    create widgets folder
    widgets that are used in other files and do not constitute a page on their own

--- client/lib/widgets/display_zone.dart
+++ client/lib/widgets/display_zone.dart
@@ -5,39 +5,47 @@ import '../models/game.dart';
 import '../providers/active_game.dart';
 import '../providers/active_play.dart';
 
+
 class PlayerScoreCards extends StatelessWidget {
-  final List<Player> players;
-  static const List<Color> colors = [
+  static const List<Color> playerColors = [
     Color.fromARGB(255, 101, 160, 255),
     Color.fromARGB(255, 69, 222, 102),
     Color.fromARGB(255, 255, 234, 0),
     Color.fromARGB(255, 221, 150, 218),
   ];
-
-  final bool displayScores;
-  const PlayerScoreCards(this.players, {this.displayScores = true, super.key});
+  final List<Player> players;
+  const PlayerScoreCards(this.players, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Selector<ActiveGame, Game>(
       selector: (_, activeGame) => activeGame.activeGame,
-      builder: (BuildContext context, Game game, Widget? child) => Consumer<ActivePlay>(
-        builder: (context, activePlay, child) {
-          return Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                for (int i = 0; i < players.length; i++)
-                  PlayerScoreCard(
-                    player: players[i],
-                    color: colors[i],
-                    isActive: players[i] == game.currentPlayer,
-                    displayScores: displayScores,
-                  ),
-              ],
-            ),
-          );
-        },
+      builder: (context, activeGame, __) => _buildWidget(activeGame),
+    );
+  }
+
+  Widget _buildWidget(Game activeGame) {
+    return Consumer<ActivePlay>(
+      builder: (_, activePlay, ___) => _build(activePlay.play!.player),
+    );
+  }
+
+  Widget _build(Player activePlayer) {
+    List<Widget> scoreCards = [];
+
+    for (int i = 0; i < players.length; i++) {
+      scoreCards.add(
+        PlayerScoreCard(
+          player: players[i],
+          color: playerColors[i],
+          isActive: players[i] == activePlayer,
+        ),
+      );
+    }
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: scoreCards,
       ),
     );
   }
@@ -47,18 +55,24 @@ class PlayerScoreCard extends StatelessWidget {
   final Player player;
   final Color color;
   final bool isActive;
-  final bool displayScores;
 
   const PlayerScoreCard({
     required this.player,
     required this.color,
     required this.isActive,
-    required this.displayScores,
     super.key,
   });
-
   @override
   Widget build(BuildContext context) {
+    List<Widget> children = [
+      Icon(isActive ? Icons.person : Icons.person_outline, color: Colors.white),
+      Text(player.name, style: Theme.of(context).textTheme.titleLarge),
+      Text(player.score.toString(), style: Theme.of(context).textTheme.bodyLarge),
+    ];
+
+    if (player.plays.isNotEmpty) {
+      children.add(MostRecentTurnDisplay(player));
+    }
     return Expanded(
       child: Container(
         padding: EdgeInsets.all(5),
@@ -69,15 +83,7 @@ class PlayerScoreCard extends StatelessWidget {
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Icon(isActive ? Icons.person : Icons.person_outline, color: Colors.white),
-            Text(player.name, style: Theme.of(context).textTheme.titleLarge),
-            if (displayScores)
-              Text(player.score.toString(), style: Theme.of(context).textTheme.bodyLarge)
-            else
-              Text(''),
-            if (player.plays.isNotEmpty) MostRecentTurnDisplay(player),
-          ],
+          children: children,
         ),
       ),
     );
@@ -93,29 +99,23 @@ class MostRecentTurnDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
     return Container(
       padding: EdgeInsets.all(5),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
+        children: [
           Text(
             'Last Turn ${lastTurn.isBingo ? '⭐️' : ''}',
-            style: Theme.of(context).textTheme.titleSmall!,
+            style: textTheme.titleSmall!,
             textAlign: TextAlign.center,
           ),
-          if (lastTurn.playedWords.isNotEmpty)
-            Text(
-              lastTurn.playedWords.map((e) => '${e.word} - ${e.score}').join('\n'),
-              style: Theme.of(context).textTheme.bodySmall!,
-              textAlign: TextAlign.start,
-            )
-          else
-            Text(
-              'Skipped',
-              style: Theme.of(context).textTheme.bodySmall!.copyWith(color: Colors.white70),
-              textAlign: TextAlign.start,
-            ),
+          Text(
+            lastTurn.playedWords.map((e) => '${e.word} - ${e.score}').join('\n'),
+            style: textTheme.bodySmall!,
+            textAlign: TextAlign.start,
+          ),
         ],
       ),
     );
