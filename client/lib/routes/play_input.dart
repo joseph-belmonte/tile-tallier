12f83b83c62b1e09e90b0e76c9eda5e24bd2fc0e
Joseph <49847370+joseph-belmonte@users.noreply.github.com>
2023-11-05 13:02:56 -0500

    Hide player score (#92)
    * enable "prefer_relative_imports"
    * rename route files
    * Delete app_state.dart
    * add display score toggle
    * Update display_zone.dart
    - show "skipped" for last turn summary when a player ends their turn without displaying a word
    * Update settings.dart
    - place in Scaffold so the content does not overlap with device feature/status bar
    * fix missing code from rebase
    * fix import route

--- client/lib/routes/play_input.dart
+++ client/lib/routes/play_input.dart
@@ -1,33 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../models/game.dart';
-import '../providers/active_game.dart';
 import '../widgets/display_zone.dart';
 import '../widgets/writing_zone.dart';
-import 'game_end/subtract.dart';
 
-class PlayInputPage extends StatelessWidget {
+import '../providers/active_game.dart';
+import '../models/game.dart';
+import './game_end/end_game.dart';
+
+class PlayInputPage extends StatefulWidget {
   static const endGameButton = EndGameButton();
+
   static const writingZone = WritingZone();
   const PlayInputPage({super.key});
+
+  @override
+  State<PlayInputPage> createState() => _PlayInputPageState();
+}
+
+class _PlayInputPageState extends State<PlayInputPage> {
+  bool displayScores = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PLAY INPUT'),
-        automaticallyImplyLeading: false,
+        title: Text('Play Input'),
       ),
       body: Selector<ActiveGame, List<Player>>(
         selector: (_, activeGame) => activeGame.players,
         builder: (_, players, __) {
           return Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              endGameButton,
-              PlayerScoreCards(players),
-              writingZone,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  PlayInputPage.endGameButton,
+                  ElevatedButton.icon(
+                    icon: displayScores ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
+                    label: Text('Scores'),
+                    onPressed: () => setState(() {
+                      displayScores = !displayScores;
+                    }),
+                  ),
+                ],
+              ),
+              PlayerScoreCards(players, displayScores: displayScores),
+              PlayInputPage.writingZone,
             ],
           );
         },
@@ -46,10 +66,20 @@ class EndGameButton extends StatelessWidget {
       builder: (context, activeGame, __) {
         return ElevatedButton.icon(
           label: Text('End Game'),
-          icon: Icon(Icons.file_download_off_rounded),
+          icon: Icon(Icons.assistant_photo_rounded),
+
+          /// Calculates the winner and navigates to the end game page.
           onPressed: () {
+            final winner = activeGame.leader;
+            final playerPositions = activeGame.getPlayersSortedByScore();
             Navigator.of(context).push(
-              MaterialPageRoute(builder: (_) => SubtractPage(game: activeGame)),
+              MaterialPageRoute(
+                builder: (_) => EndGamePage(
+                  winner: winner,
+                  rankedPlayers: playerPositions,
+                  game: activeGame,
+                ),
+              ),
             );
           },
         );
