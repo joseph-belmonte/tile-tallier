ca22db056092cf7a5aeaada78557decd47a761c2
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-06-10 22:10:40 -0400

    handle requests/responses

--- server/tt_backend/snippets/urls.py
+++ server/tt_backend/snippets/urls.py
@@ -1,9 +1,12 @@
 """ This file is used to define the URL patterns for the snippets app. """
 
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path("snippets/", views.snippet_list),
     path("snippets/<int:pk>/", views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
