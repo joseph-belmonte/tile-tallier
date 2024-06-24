7f82ce72374e58fdd32ff729a80039fa0a526180
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-06-24 17:24:48 -0400

    Update admin.py
    remove admin dashboard

--- server/tt_backend/accounts/admin.py
+++ server/tt_backend/accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from accounts.models import User
+
+admin.site.unregister(Group)
 
 
 # Register your models here.
