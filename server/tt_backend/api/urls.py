ba1fc56b130786868c36e1533edb7a8400a563e2
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-06-25 17:44:09 -0400

    update url schema
    all paths go through api. examples:
    - api/account/register
    - api/game/can_play
    - api/auth/token/refresh

--- server/tt_backend/api/urls.py
+++ server/tt_backend/api/urls.py
@@ -3,4 +3,5 @@ from django.urls import include, path
 urlpatterns = [
     path("game/", include("api.game.urls")),
     path("auth/", include("api.auth.urls")),
+    path("account/", include("api.account.urls")),
 ]
