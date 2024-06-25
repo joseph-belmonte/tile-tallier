9baa0b4cf61c12a61e0b586727a284ac9f9a4903
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-06-25 17:45:57 -0400

    Update views.py

--- server/tt_backend/api/game/views.py
+++ server/tt_backend/api/game/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.views import TokenObtainPairView
 from content.models import GamePlay
 
 
