957ce938e544d2810cecfd3d16f7a52091500205
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-06-25 17:50:53 -0400

    move serializers to API folder

--- server/tt_backend/api/account/serializers.py
+++ server/tt_backend/api/account/serializers.py
@@ -1,23 +1,30 @@
-# accounts/serializers.py
 from rest_framework import serializers
-from .models import User
-from content.models import GamePlay
+from accounts.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class AccountRegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ["email", "password", "password2", "is_subscribed", "purchases"]
+        fields = ["email", "password", "password2", "is_subscribed"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def validate(self, data):
         if data["password"] != data["password2"]:
             raise serializers.ValidationError("Passwords do not match")
+
+        if User.objects.filter(email=data["email"]).exists():
+            raise serializers.ValidationError("Email is already in use")
+
         return data
 
     def create(self, validated_data):
         validated_data.pop("password2")
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class AccountLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
