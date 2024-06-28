1ad29d45bdf9504522ac56d4ac7ae2dbcd26750f
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-06-28 12:39:36 -0400

    revise user model (auth)

--- client/lib/features/auth/domain/models/user.dart
+++ client/lib/features/auth/domain/models/user.dart
@@ -1,25 +1,26 @@
+// models/user.dart
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
 /// A user model.
-class User {
-  /// the id of the user.
-  final bool? isAuthenticated;
+@freezed
+class User with _$User {
+  /// Creates a new [User] instance.
+  const factory User({
+    @Default(false) bool isAuthenticated,
+    @Default('') String email,
+    @Default(false) bool isSubscribed,
+  }) = _User;
 
-  /// the email of the user.
-  final String? email;
-
-  /// Whether the user is subscribed.
-  final bool? isSubscribed;
+  /// Converts the user to a map.
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 
   /// Default constructor for the user model.
-  User({
-    required this.isAuthenticated,
-    required this.email,
-    required this.isSubscribed,
-  });
-
-  /// Converts a JSON object to a user model.
-  factory User.fromJson(Map<String, dynamic> json) => User(
-        isAuthenticated: json['is_authenticated'],
-        email: json['email'],
-        isSubscribed: json['is_subscribed'],
+  factory User.initial() => const User(
+        isAuthenticated: false,
+        email: '',
+        isSubscribed: false,
       );
 }
