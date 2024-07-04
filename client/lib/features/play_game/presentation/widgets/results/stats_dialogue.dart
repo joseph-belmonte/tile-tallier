4410bd210550aa82348700204b6342bf883060f6
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-07-04 13:04:09 -0400

    Update stats_dialogue.dart

--- client/lib/features/play_game/presentation/widgets/results/stats_dialogue.dart
+++ client/lib/features/play_game/presentation/widgets/results/stats_dialogue.dart
@@ -18,70 +18,44 @@ class StatsDialog extends StatelessWidget {
     return AlertDialog(
       backgroundColor: Theme.of(context).colorScheme.surfaceContainerHighest,
       title: Text(
-        'Notable Stats',
+        'Game Summary',
         style: Theme.of(context).textTheme.headlineSmall!.copyWith(
               color: Theme.of(context).colorScheme.onSurfaceVariant,
             ),
       ),
       content: SingleChildScrollView(
+        primary: true,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Text(
-                'Total Plays: ${_game.plays.length}',
-              ),
+            Text('Winner: ${_game.sortedPlayers.first.name}'),
+            Text('Total Plays: ${_game.plays.length}'),
+            SizedBox(height: 16),
+            Text('Longest Word:'),
+            Text(_game.longestWord),
+            SizedBox(height: 16),
+            Text('Highest Scoring Word:'),
+            Text(
+              '${_game.highestScoringWord.word} - ${_game.highestScoringWord.score}',
             ),
-            Divider(),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                'Longest Word: ${_game.longestWord}, for ${_game.longestWord.length} letters',
-              ),
-            ),
-            Divider(),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                'Highest Scoring Word:\n${_game.highestScoringWord.word} - ${_game.highestScoringWord.score}',
-              ),
-            ),
-            Divider(),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Text('Highest Scoring Turn:'),
-            ),
+            SizedBox(height: 16),
+            Text('Highest Scoring Play:'),
             if (topPlay != null)
-              if (topPlay.isBingo)
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  child: Text(
-                    'Bingo! +50 points',
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Player: ${_game.players.where((player) => player.id == topPlay.playerId).first.name}',
                   ),
-                ),
-            if (topPlay != null)
-              ...topPlay.playedWords.map(
-                (word) => Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  child: Text(
-                    '${word.word} - ${word.score}',
+                  Text(
+                    'Play Score: ${topPlay.score} ${topPlay.isBingo ? '- Bingo!' : ''}',
                   ),
-                ),
-              ),
-            if (topPlay != null)
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: Text(
-                  'Total: ${topPlay.score}',
-                ),
-              ),
-            if (topPlay != null)
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: Text(
-                  'Score multipliers: ${topPlay.playedWords.map((word) => word.playedLetters.map((letter) => letter.scoreMultiplier.name).where((name) => name != 'none').join(', ')).join(', ')}',
-                ),
+                  SizedBox(height: 8),
+                  Text('Played Words:'),
+                  ...topPlay.playedWords.map(
+                    (word) => Text('${word.word} - ${word.score}'),
+                  ),
+                ],
               ),
           ],
         ),
@@ -89,13 +63,13 @@ class StatsDialog extends StatelessWidget {
       actions: <Widget>[
         TextButton(
           style: TextButton.styleFrom(
-            backgroundColor: Theme.of(context).colorScheme.surface,
+            backgroundColor: Theme.of(context).colorScheme.primary,
           ),
           onPressed: () => Navigator.of(context).pop(),
           child: Text(
             'Close',
             style: TextStyle(
-              color: Theme.of(context).colorScheme.onSurface,
+              color: Theme.of(context).colorScheme.onPrimary,
             ),
           ),
         ),
