1a9fd01b31d6f0f79c68c73dd749dc7e90a86a48
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-07-10 10:33:35 -0400

    Update serialization_test.dart

--- client/test/unit/serialization_test.dart
+++ client/test/unit/serialization_test.dart
@@ -1,28 +1,27 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tile_tally/features/core/domain/models/letter.dart';
+import 'package:tile_tally/features/core/domain/models/play.dart';
 import 'package:tile_tally/features/core/domain/models/word.dart';
-import 'package:tile_tally/utils/logger.dart';
 
 void main() {
-  test('Serialization is working correctly', () {
+  test('Play JSON serialization and deserialization', () {
     final letters = [
       Letter.createNew(letter: 'A'),
       Letter.createNew(letter: 'B'),
       Letter.createNew(letter: 'C'),
     ];
 
-    final letter = letters[0];
-    final serializedLetter = letter.toJson();
-    logger.d('Serialized Letter: $serializedLetter');
-    final deserializedLetter = Letter.fromJson(serializedLetter);
-    logger.d('Deserialized Letter: $deserializedLetter');
-    expect(deserializedLetter, equals(letter));
-
     final word = Word.createNew().copyWith(playedLetters: letters);
-    final serializedWord = word.toJson();
-    logger.d('Serialized Word: $serializedWord');
-    final deserializedWord = Word.fromJson(serializedWord);
-    logger.d('Deserialized Word: $deserializedWord');
-    expect(deserializedWord, equals(word));
+    final play = Play.createNew(gameId: 'game_1').copyWith(
+      playedWords: [word],
+    );
+
+    final playJson = play.toJson();
+    print('Serialized Play: $playJson');
+
+    final deserializedPlay = Play.fromJson(playJson);
+    print('Deserialized Play: $deserializedPlay');
+
+    expect(deserializedPlay, play);
   });
 }
