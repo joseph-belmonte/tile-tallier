32d00f654f9be2290822a9937488ad1ca6681782
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-07-15 15:05:04 -0400

    add mock data

---
+++ client/test/mocks/data/games.dart
@@ -0,0 +1,41 @@
+import 'package:tile_tally/features/core/domain/models/game.dart';
+
+final game1 = Game(
+  id: 'game1',
+  isFavorite: false,
+  players: [],
+  currentPlay: null,
+  currentWord: null,
+);
+
+final game2 = Game(
+  id: 'game2',
+  isFavorite: false,
+  players: [],
+  currentPlay: null,
+  currentWord: null,
+);
+
+final game3 = Game(
+  id: 'game3',
+  isFavorite: false,
+  players: [],
+  currentPlay: null,
+  currentWord: null,
+);
+
+final game4 = Game(
+  id: 'game4',
+  isFavorite: false,
+  players: [],
+  currentPlay: null,
+  currentWord: null,
+);
+
+final game5 = Game(
+  id: 'game5',
+  isFavorite: false,
+  players: [],
+  currentPlay: null,
+  currentWord: null,
+);
