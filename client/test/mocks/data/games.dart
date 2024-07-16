5499cfb3193db0952f457ba45a6e110840ead470
Joseph <49847370+joseph-belmonte@users.noreply.github.com>
2024-07-16 12:01:59 -0400

    Add serialization tests for models. (#39)
    * Update serialization_test.dart
    
    * Update games.dart
    
    add plays and words and letter to mock game
    
    * add serialization tests for missing models

--- client/test/mocks/data/games.dart
+++ client/test/mocks/data/games.dart
@@ -1,4 +1,8 @@
 import 'package:tile_tally/features/core/domain/models/game.dart';
+import 'package:tile_tally/features/core/domain/models/game_player.dart';
+import 'package:tile_tally/features/core/domain/models/letter.dart';
+import 'package:tile_tally/features/core/domain/models/play.dart';
+import 'package:tile_tally/features/core/domain/models/word.dart';
 
 final game1 = Game(
   id: 'game1',
@@ -36,6 +40,63 @@ final game5 = Game(
   id: 'game5',
   isFavorite: false,
   players: [],
+  currentPlay: null,
+  currentWord: null,
+);
+
+final game6 = Game(
+  id: 'game6',
+  isFavorite: false,
+  players: [
+    GamePlayer(
+      id: 'player1',
+      playerId: 'player1',
+      gameId: 'game6',
+      name: 'Andrea',
+      plays: <Play>[
+        Play(
+          id: 'play1',
+          gameId: 'game6',
+          timestamp: DateTime.now(),
+          playedWords: <Word>[
+            Word(
+              id: 'word1',
+              playedLetters: <Letter>[
+                Letter(letter: 'A', id: 'a'),
+                Letter(letter: 'B', id: 'b'),
+                Letter(letter: 'C', id: 'c'),
+              ],
+            ),
+          ],
+        ),
+      ],
+      endRack: '',
+    ),
+    GamePlayer(
+      id: 'player2',
+      playerId: 'player2',
+      gameId: 'game6',
+      name: 'Joe',
+      plays: <Play>[
+        Play(
+          id: 'play2',
+          gameId: 'game6',
+          timestamp: DateTime.now(),
+          playedWords: <Word>[
+            Word(
+              id: 'word2',
+              playedLetters: <Letter>[
+                Letter(letter: 'D', id: 'd'),
+                Letter(letter: 'E', id: 'e'),
+                Letter(letter: 'F', id: 'f'),
+              ],
+            ),
+          ],
+        ),
+      ],
+      endRack: '',
+    ),
+  ],
   currentPlay: null,
   currentWord: null,
 );
--- client/test/mocks/data/players.dart
+++ client/test/mocks/data/players.dart
@@ -5,4 +5,4 @@ final player2 = Player(id: 'player2', name: 'Jane Doe');
 final player3 = Player(id: 'player3', name: 'Alice');
 final player4 = Player(id: 'player4', name: 'Bob');
 final player5 = Player(id: 'player5', name: 'Charlie');
-final player = Player(id: 'player6', name: 'Dave');
+final player6 = Player(id: 'player6', name: 'Dave');
