f8b5ee0abbf74d1a63423508e92c29a94e61eb70
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-07-17 11:29:16 -0400

    Update player_input_fields.dart

--- client/lib/features/play_game/presentation/widgets/pregame/player_input_fields.dart
+++ client/lib/features/play_game/presentation/widgets/pregame/player_input_fields.dart
@@ -18,48 +18,85 @@ class PlayerInputFields extends ConsumerWidget {
     return ListView.builder(
       shrinkWrap: true,
       itemCount: ref.read(preGameProvider).playerCount,
-      itemBuilder: (context, index) => Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 40.0),
-        child: Column(
-          children: <Widget>[
-            Focus(
-              onFocusChange: (hasFocus) {
-                if (hasFocus) {
-                  ref.read(preGameProvider.notifier).setActiveField(index);
-                }
-              },
-              child: TextFormField(
-                controller: ref.read(preGameProvider).controllers[index],
-                decoration: InputDecoration(
-                  labelText: 'Player ${index + 1} Name',
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.clear),
-                    onPressed: () {
-                      ref.read(preGameProvider).controllers[index].clear();
-                    },
-                  ),
-                ),
-                validator: (value) => (value == null || value.trim().isEmpty)
-                    ? 'Please enter a name'
-                    : null,
-                onChanged: (value) {
-                  if (ref
-                      .read(preGameProvider.notifier)
-                      .playerNames
-                      .contains(value)) {
-                    ToastService.error(context, 'Player already selected');
-                  } else {
-                    ref
-                        .read(preGameProvider.notifier)
-                        .updatePlayerName(index, value);
+      itemBuilder: (context, index) {
+        final playerNames = ref.watch(preGameProvider).selectedPlayers;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 40.0),
+          child: Column(
+            children: <Widget>[
+              Focus(
+                onFocusChange: (hasFocus) {
+                  if (hasFocus) {
+                    ref.read(preGameProvider.notifier).setActiveField(index);
                   }
                 },
-                inputFormatters: [LengthLimitingTextInputFormatter(20)],
+                child: TextFormField(
+                  controller: ref.read(preGameProvider).controllers[index],
+                  decoration: InputDecoration(
+                    labelText: 'Player ${index + 1} Name',
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.clear),
+                      onPressed: () {
+                        final name = ref
+                            .read(preGameProvider)
+                            .controllers[index]
+                            .text
+                            .trim();
+
+                        if (name.isEmpty) return;
+
+                        // Remove the player from the game if they were selected.
+                        ref.read(preGameProvider.notifier).deselectPlayer(name);
+
+                        // Clear the text controller
+                        ref.read(preGameProvider).controllers[index].clear();
+                        ToastService.message(
+                          context,
+                          '$name removed from game',
+                        );
+                      },
+                    ),
+                  ),
+                  validator: (value) {
+                    return (value == null || value.trim().isEmpty)
+                        ? 'Please enter a name'
+                        : null;
+                  },
+                  onChanged: (value) {
+                    final hasKnownPlayers =
+                        ref.watch(preGameProvider).knownPlayers.isNotEmpty;
+                    final nameMatches =
+                        playerNames.any((name) => name == value);
+                    var alreadyUsed = false;
+                    for (var i = 0;
+                        i < ref.watch(preGameProvider).controllers.length;
+                        i++) {
+                      if (i != index &&
+                          ref.watch(preGameProvider).controllers[i].text ==
+                              value) {
+                        alreadyUsed = true;
+                        break;
+                      }
+                    }
+
+                    if (hasKnownPlayers) {
+                      if (nameMatches || alreadyUsed) {
+                        ToastService.error(context, 'Player already selected');
+                      }
+                    } else {
+                      ref
+                          .read(preGameProvider.notifier)
+                          .updatePlayerName(index, value);
+                    }
+                  },
+                  inputFormatters: [LengthLimitingTextInputFormatter(20)],
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
