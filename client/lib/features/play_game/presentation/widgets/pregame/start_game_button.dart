c3e2207401e39283169ad5bc6152891a7480ab42
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-07-18 15:33:23 -0400

    Update start_game_button.dart

--- client/lib/features/play_game/presentation/widgets/pregame/start_game_button.dart
+++ client/lib/features/play_game/presentation/widgets/pregame/start_game_button.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: public_member_api_docs
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class StartGameButton extends StatelessWidget {
+import '../../../../../utils/toast.dart';
+import '../../controllers/pre_game_controller.dart';
+
+class StartGameButton extends ConsumerWidget {
   final GlobalKey<FormState> formKey;
   final VoidCallback onPressed;
 
@@ -13,13 +17,17 @@ class StartGameButton extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 20.0),
       child: ElevatedButton.icon(
         onPressed: () {
           if (formKey.currentState!.validate()) {
+            ref.read(preGameProvider).hasError = false;
             onPressed();
+          } else {
+            ref.read(preGameProvider).hasError = true;
+            ToastService.error(context, 'Please review name fields');
           }
         },
         icon: const Icon(Icons.play_arrow_rounded, semanticLabel: 'start game'),
