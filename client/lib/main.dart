da3fd930040b47ac0df25d97036a5ef4e2bd90bc
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-07-17 21:46:54 -0400

    extract initialization logic from main.dart

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,38 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'features/edit_settings/application/providers/key_value_db_listener.dart';
-import 'features/edit_settings/application/providers/key_value_db_provider.dart';
-import 'features/manage_purchases/application/configure_rc_SDK.dart';
-import 'features/manage_purchases/application/providers/customer_info_provider.dart';
-import 'features/play_game/data/word_database_helper.dart';
 import 'theme/theme_wrapper.dart';
+import 'utils/start/app_initializations.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final container = ProviderContainer();
+  final settingsContainer = ProviderContainer();
 
-  // Get default keyValueDb implementation and initialize it for use.
-  await container.read(keyValueDbProvider).init();
-
-  // By reading the keyValueDbListenerProvider, we instantiate it. This sets up
-  // a listener that listens to state changes in keyValueDbProvider. In the
-  // listener we use SharedPreferences to save the state of the keyValueDbProvider.
-  container.read(keyValueDbListenerProvider);
-
-  // Check if the database is populated. If not, import the word list.
-  if (!await WordListDBHelper.instance.isDatabasePopulated()) {
-    await WordListDBHelper.instance.importWordList();
-  }
-
-  await configureRcSdk();
-
-  container.read(customerInfoProvider.notifier);
+  await initializeApp(settingsContainer);
 
   runApp(
     UncontrolledProviderScope(
-      container: container,
+      container: settingsContainer,
       child: ProviderScope(
         child: const ThemeWrapper(),
       ),
