ba19887a91ce015e3b9c241e6c75d9e27dcba026
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-07-20 11:20:37 -0400

    Update turn_hud.dart
    - hide play based attributes from single word plays
    - add animation to star

--- client/lib/features/play_game/presentation/widgets/gameplay/turn_hud.dart
+++ client/lib/features/play_game/presentation/widgets/gameplay/turn_hud.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,25 +15,54 @@ class TurnHUD extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final game = ref.watch(activeGameProvider);
     final notifier = ref.read(activeGameProvider.notifier);
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Text('Current: ${game.currentPlayer.name}'),
-          Text('Play Score: ${game.currentPlay!.score}'),
-          Text('Word Score: ${game.currentWord!.score}'),
-          IconButton(
-            onPressed: notifier.toggleBingo,
-            icon: game.currentPlay!.isBingo
-                ? Icon(Icons.star, semanticLabel: 'Play is bingo')
-                : Icon(Icons.star_border, semanticLabel: 'Play is not bingo'),
-            iconSize: 32.0,
-          ),
-          Text(
-            'Played Words: ${game.currentPlay!.playedWords.map((word) => word.word).join(', ')}',
-          ),
-        ],
+    return Container(
+      key: const Key('turn_hud'),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.inversePrimary,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              game.currentPlayer.name,
+              style: Theme.of(context).textTheme.headlineSmall!,
+            ),
+            if (game.currentPlay!.playedWords.isNotEmpty ||
+                game.currentPlay!.isBingo)
+              Text('Play Score: ${game.currentPlay!.score}'),
+            Text(
+              'Word Score: ${game.currentWord!.score}',
+              style: Theme.of(context).textTheme.bodyMedium!,
+            ),
+            IconButton(
+              onPressed: notifier.toggleBingo,
+              icon: game.currentPlay!.isBingo
+                  ? Spin(
+                      duration: Durations.long1,
+                      key: const Key('bingo'),
+                      child: Icon(Icons.star, semanticLabel: 'Play is bingo'),
+                    )
+                  : Icon(Icons.star_border, semanticLabel: 'Play is not bingo'),
+              iconSize: 44.0,
+            ),
+            if (game.currentPlay?.playedWords.isNotEmpty ?? false) ...[
+              const SizedBox(height: 8.0),
+              Text(
+                'Played Words:',
+                style: Theme.of(context).textTheme.bodyMedium!,
+              ),
+              Text(
+                game.currentPlay!.playedWords
+                    .map((word) => word.word)
+                    .join(', '),
+                style: Theme.of(context).textTheme.bodyMedium!,
+              ),
+            ],
+          ],
+        ),
       ),
     );
   }
