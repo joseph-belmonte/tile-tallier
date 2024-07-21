84d5223767cb731e8ca12315d3da0af2ec6aff51
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-07-21 15:11:15 -0400

    Update turn_hud.dart
    fix spin to work for favorite + unfavorite

--- client/lib/features/play_game/presentation/widgets/gameplay/turn_hud.dart
+++ client/lib/features/play_game/presentation/widgets/gameplay/turn_hud.dart
@@ -7,14 +7,18 @@ import '../../../application/providers/active_game.dart';
 /// A widget to display the current turn information.
 class TurnHUD extends ConsumerWidget {
   /// Creates a new [TurnHUD] instance.
-  const TurnHUD({
-    super.key,
-  });
+  const TurnHUD({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    const starSpins = 0.6;
+    const animationDuration = Durations.long4;
+
+    void handleFavorite() {
+      ref.read(activeGameProvider.notifier).toggleBingo();
+    }
+
     final game = ref.watch(activeGameProvider);
-    final notifier = ref.read(activeGameProvider.notifier);
     return Container(
       key: const Key('turn_hud'),
       decoration: BoxDecoration(
@@ -33,32 +37,34 @@ class TurnHUD extends ConsumerWidget {
             if (game.currentPlay!.playedWords.isNotEmpty ||
                 game.currentPlay!.isBingo)
               Text('Play Score: ${game.currentPlay!.score}'),
-            Text(
-              'Word Score: ${game.currentWord!.score}',
-              style: Theme.of(context).textTheme.bodyMedium!,
-            ),
+            Text('Word Score: ${game.currentWord!.score}'),
             IconButton(
-              onPressed: notifier.toggleBingo,
-              icon: game.currentPlay!.isBingo
+              onPressed: handleFavorite,
+              icon: ref.watch(activeGameProvider).currentPlay!.isBingo
                   ? Spin(
-                      duration: Durations.long1,
+                      duration: animationDuration,
+                      spins: starSpins,
                       key: const Key('bingo'),
                       child: Icon(Icons.star, semanticLabel: 'Play is bingo'),
                     )
-                  : Icon(Icons.star_border, semanticLabel: 'Play is not bingo'),
+                  : Spin(
+                      duration: animationDuration,
+                      spins: -starSpins,
+                      key: const Key('no_bingo'),
+                      child: Icon(
+                        Icons.star_border,
+                        semanticLabel: 'Play is not bingo',
+                      ),
+                    ),
               iconSize: 44.0,
             ),
-            if (game.currentPlay?.playedWords.isNotEmpty ?? false) ...[
+            if (game.currentPlay?.playedWords.isNotEmpty ?? false) ...<Widget>[
               const SizedBox(height: 8.0),
-              Text(
-                'Played Words:',
-                style: Theme.of(context).textTheme.bodyMedium!,
-              ),
+              Text('Played Words:'),
               Text(
                 game.currentPlay!.playedWords
                     .map((word) => word.word)
                     .join(', '),
-                style: Theme.of(context).textTheme.bodyMedium!,
               ),
             ],
           ],
