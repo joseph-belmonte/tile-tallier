4af823179d9acae9530293852ddf6e6695afd5e7
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-08-02 20:10:00 -0400

    Update word_database_helper.dart
    add logic for themed word tables

--- client/lib/features/play_game/data/word_database_helper.dart
+++ client/lib/features/play_game/data/word_database_helper.dart
@@ -3,6 +3,8 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
+import '../../../enums/word_theme.dart';
+
 /// A helper class to manage the word_database file.
 /// It mainly contains the word_list table.
 class WordListDBHelper {
@@ -11,6 +13,31 @@ class WordListDBHelper {
 
   /// The default table name
   static const defaultTable = 'word_list';
+
+  /// The wordle table name
+  static const wordleTable = 'wordle_list';
+
+  /// The family table name
+  static const familyTable = 'family_list';
+
+  /// The avian table name
+  static const avianTable = 'avian_list';
+
+  /// The maritime table name
+  static const maritimeTable = 'maritime_list';
+
+  /// The arbor table name
+  static const arborTable = 'arbor_list';
+
+  /// A list of all the tables in the database
+  final tables = [
+    defaultTable,
+    wordleTable,
+    familyTable,
+    avianTable,
+    maritimeTable,
+    arborTable,
+  ];
 
   /// The id column name
   static const columnId = 'id';
@@ -46,72 +73,130 @@ class WordListDBHelper {
 
   // SQL code to create the database table
   Future _onCreate(Database db, int version) async {
-    await db.execute('''
-          CREATE TABLE $defaultTable (
-            $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
-            $columnWord TEXT NOT NULL
-          )
-          ''');
+    await db.transaction((txn) async {
+      for (var table in tables) {
+        await txn.execute('''
+        CREATE TABLE $table (
+          $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+          $columnWord TEXT NOT NULL
+        )
+      ''');
+      }
+    });
   }
 
-  /// Insert a row into the database
-  Future<int> insert(Map<String, dynamic> row, {Database? txn}) async {
+  // Method to get table name and path based on the theme
+  Map<String, String> _getTableNameAndPath(WordTheme theme) {
+    switch (theme) {
+      case WordTheme.basic:
+        return {'tableName': defaultTable, 'path': WordTheme.basic.path};
+      case WordTheme.wordle:
+        return {'tableName': wordleTable, 'path': WordTheme.wordle.path};
+      case WordTheme.family:
+        return {'tableName': familyTable, 'path': WordTheme.family.path};
+      case WordTheme.avian:
+        return {'tableName': avianTable, 'path': WordTheme.avian.path};
+      case WordTheme.maritime:
+        return {'tableName': maritimeTable, 'path': WordTheme.maritime.path};
+      case WordTheme.arbor:
+        return {'tableName': arborTable, 'path': WordTheme.arbor.path};
+      default:
+        throw Exception('Cannot find word list for ${theme.name}');
+    }
+  }
+
+  /// Insert a row into the specified table
+  Future<int> insert({
+    required String tableName,
+    required Map<String, dynamic> row,
+    Database? txn,
+  }) async {
     final db = txn ?? await instance.database;
-    return await db.insert(defaultTable, row);
+    return await db.insert(tableName, row);
   }
 
   /// Query all rows in the table
-  Future<List<Map<String, dynamic>>> queryAllRows({Database? txn}) async {
+  Future<List<Map<String, dynamic>>> queryAllRows({
+    required String tableName,
+    Database? txn,
+  }) async {
     final db = txn ?? await instance.database;
-    return await db.query(defaultTable);
+    return await db.query(tableName);
   }
 
   /// Queries the number of rows in the table
-  Future<int?> queryRowCount({Database? txn}) async {
+  Future<int?> queryRowCount({required WordTheme theme, Database? txn}) async {
     final db = txn ?? await instance.database;
+
+    // Given the theme, find the tableName
+    final tableName = _getTableNameAndPath(theme)['tableName']!;
+
     return Sqflite.firstIntValue(
-      await db.rawQuery('SELECT COUNT(*) FROM $defaultTable'),
+      await db.rawQuery('SELECT COUNT(*) FROM $tableName'),
     );
   }
 
-  /// Delete all rows in the table
-  Future<int> deleteAll({Database? txn}) async {
+  /// Delete all rows in the specified table
+  Future<int> deleteAll({required String tableName, Database? txn}) async {
     final db = txn ?? await instance.database;
-    return await db.delete(defaultTable);
+    return await db.delete(tableName);
   }
 
-  /// Loads the 2006 tournament word list.
-  Future<void> importWordList() async {
-    final wordListString =
-        await rootBundle.loadString('assets/txt/word_list.txt');
+  /// Loads the word list at a given path and inserts it into the 'word_database.db
+  Future<void> populateTable({required WordTheme theme}) async {
+    final tableInfo = _getTableNameAndPath(theme);
+    final path = tableInfo['path']!;
+    final tableName = tableInfo['tableName']!;
+
+    final wordListString = await rootBundle.loadString(path);
     final wordList = wordListString.split('\n');
     final batch = _database!.batch();
     for (var word in wordList) {
       if (word.trim().isNotEmpty) {
-        batch.insert(defaultTable, {columnWord: word});
+        batch.insert(tableName, {columnWord: word.trim().toLowerCase()});
       }
     }
     await batch.commit(noResult: true);
   }
 
+  /// Get a list of words from the database
+  Future<List<String>> getWordList(WordTheme theme) async {
+    final db = await database;
+
+    final tableName = _getTableNameAndPath(theme)['tableName']!;
+
+    final results = await db.query(tableName, columns: [columnWord]);
+    return results.map((row) => row[columnWord] as String).toList();
+  }
+
   /// Whether any of the given words exist in the database
-  Future<bool> wordExistsInList(List<String> words) async {
+  Future<bool> wordExistsInList(
+    List<String> words, {
+    String tableName = defaultTable,
+  }) async {
     final db = await database;
     final placeholders = List.filled(words.length, '?').join(', ');
-    final query = 'SELECT 1 FROM $defaultTable WHERE word IN ($placeholders) LIMIT 1';
+    final query =
+        'SELECT 1 FROM $tableName WHERE word IN ($placeholders) LIMIT 1';
     final results = await db.rawQuery(query, words);
     return results.isNotEmpty;
   }
 
-  /// Whether the database is already populated
+  /// Whether each table in the database in populated
   Future<bool> isDatabasePopulated() async {
-    final db = await database;
-    final count =
-        Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM $defaultTable'));
-    if (count != null && count > 0) {
+    try {
+      final db = await database;
+      for (var table in tables) {
+        final count = Sqflite.firstIntValue(
+          await db.rawQuery('SELECT COUNT(*) FROM $table'),
+        );
+        if (count == null || count == 0) {
+          return false;
+        }
+      }
       return true;
-    } else {
-      return false;
+    } catch (e) {
+      throw 'Error checking if database is populated: $e';
     }
   }
 
