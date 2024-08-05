a7444939263b82152c1a2338e21d8c020f6b6f7e
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-08-05 13:38:46 -0400

    extract error navigation function

--- client/lib/utils/errors.dart
+++ client/lib/utils/errors.dart
@@ -1,5 +1,10 @@
 // ignore_for_file: public_member_api_docs
 
+import 'package:flutter/material.dart';
+
+import '../features/shared/presentation/screens/error_page.dart';
+import '../main.dart';
+
 class AppException implements Exception {
   final String message;
 
@@ -20,4 +25,18 @@ class AuthException extends AppException {
 
 class ValidationException extends AppException {
   ValidationException(super.message);
+}
+
+/// Handles the error and navigates to the error page.
+void handleError(String error, String stackTrace) {
+  WidgetsBinding.instance.addPostFrameCallback((_) {
+    navigatorKey.currentState?.push(
+      MaterialPageRoute(
+        builder: (_) => ErrorPage(
+          errorMessage: error,
+          stackTrace: stackTrace,
+        ),
+      ),
+    );
+  });
 }
