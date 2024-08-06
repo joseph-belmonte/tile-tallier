745c614534510821181b222386a22d2db560085e
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-08-05 23:04:49 -0400

    rename history db provider

--- client/lib/features/history/application/providers/history_repository_provider.dart
+++ client/lib/features/history/application/providers/history_repository_provider.dart
@@ -5,6 +5,6 @@ import 'database_provider.dart';
 
 /// A provider that controls interactions with the [HistoryRepository].
 final historyRepositoryProvider = Provider<HistoryRepository>((ref) {
-  final databaseFuture = ref.watch(databaseProvider.future);
+  final databaseFuture = ref.watch(historyDatabaseProvider.future);
   return HistoryRepository(databaseFuture);
 });
