8d62add1e122f543433a49c14a63b35f757928d8
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2024-08-06 20:25:58 -0400

    add confetti on results page load

--- client/lib/features/play_game/presentation/screens/results.dart
+++ client/lib/features/play_game/presentation/screens/results.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../../core/domain/models/game.dart';
 import '../../../shared/presentation/widgets/share_modal.dart';
 import '../widgets/home_button.dart';
+import '../widgets/results/confetti.dart';
 import '../widgets/results/player_rankings.dart';
 import '../widgets/results/show_stats_button.dart';
 import '../widgets/results/winner_header.dart';
@@ -10,15 +11,15 @@ import '../widgets/results/winner_header.dart';
 /// A page that displays the game statistics and the winner of the game.
 class ResultsPage extends StatelessWidget {
   /// Creates a new [ResultsPage] instance.
-  const ResultsPage({
-    required Game game,
-    super.key,
-  }) : _game = game;
+  const ResultsPage({required Game game, super.key}) : _game = game;
 
   final Game _game;
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      launchConfetti(context);
+    });
     return Scaffold(
       appBar: AppBar(
         title: Text('GAME OVER'),
